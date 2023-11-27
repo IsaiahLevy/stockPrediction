f82e4f91efd5f40ec0ca74cb749348c87dc97da2
IsaiahLevy <il1016@nyu.edu>
2023-11-27 18:39:57 -0500

    updated stock data

--- data/stockData.js
+++ data/stockData.js
@@ -1,66 +1,66 @@
 const fs = require('fs');
+const path = require('path');
 
 async function fetchWithImport() {
     const fetch = (await import('node-fetch')).default;
     return fetch;
 }
 
-async function getStockPrices(symbol, date) {
+async function getStockPrices(symbol) {
     const fetch = await fetchWithImport();
-    const API_TOKEN = '5Ul0vTB1p5tLrLQnxxYYMaofc7zwhK8oM6b4AXKY';
-    const BASE_URL = 'https://api.stockdata.org/v1/data/eod';
-    const url = `${BASE_URL}?symbols=${symbol}&api_token=${API_TOKEN}&date=${date}`;
+    const API_KEY = 'TZZ5DWX3FW39H8RE';  // Replace with your actual API Key
+    const BASE_URL = 'https://www.alphavantage.co/query';
+    const url = `${BASE_URL}?function=TIME_SERIES_DAILY&symbol=${symbol}&outputsize=full&apikey=${API_KEY}`;
+
     try {
         const response = await fetch(url);
         const data = await response.json();
-        console.log('API Response for', symbol, 'on', date, ':', data);
         return data;
     } catch (error) {
-        console.error('Error fetching stock data for', symbol, 'on', date, ':', error);
+        console.error('Error fetching stock data for', symbol, ':', error);
         return null;
     }
 }
 
-// Read existing data from file
-let stockDataResults = [];
-try {
-    stockDataResults = JSON.parse(fs.readFileSync('stockData.json', 'utf8'));
-} catch (error) {
-    console.log('No existing data file found, starting fresh.');
-}
-
-const companies = ['AAPL', 'MSFT', 'AMZN', 'GOOGL'];
+const stockDataResults = [];
 
 async function main() {
+    const companies = ['AAPL', 'MSFT', 'AMZN', 'GOOGL'];
+    const startDate = new Date('2022-05-01');
+    const endDate = new Date('2022-11-30');
+
     for (const company of companies) {
-        for (let month = 5; month <= 10; month++) {
-            for (let day = 1; day <= 31; day++) {
-                const date = `2022-${month.toString().padStart(2, '0')}-${day.toString().padStart(2, '0')}`;
-                
-                if (!stockDataResults.some(data => data.company === company && data.date === date)) {
-                    const stockData = await getStockPrices(company, date);
-                    if (stockData && stockData.data && stockData.data.length > 0) {
-                        const formattedData = {
-                            company: company,
-                            date: date,
-                            high: stockData.data[0].high,
-                            low: stockData.data[0].low,
-                            volume: stockData.data[0].volume,
-                            dayChange: stockData.data[0].day_change,
-                            week52High: stockData.data[0]['52_week_high'],
-                            week52Low: stockData.data[0]['52_week_low'],
-                            marketCap: stockData.data[0].market_cap,
-                            previousClosePrice: stockData.data[0].previous_close_price
-                        };
-                        
-                        stockDataResults.push(formattedData);
-                    }
+        const stockData = await getStockPrices(company);
+        if (stockData && stockData['Time Series (Daily)']) {
+            const sortedDates = Object.keys(stockData['Time Series (Daily)']).sort((a, b) => new Date(b) - new Date(a));
+
+            for (const date of sortedDates) {
+                const dateObj = new Date(date);
+                if (dateObj < startDate) {
+                    break;  // Skip dates older than the start date
                 }
-                // Save data to file after each new entry
-                fs.writeFileSync('stockData.json', JSON.stringify(stockDataResults, null, 2));
+                if (dateObj <= endDate) {
+                    const priceData = stockData['Time Series (Daily)'][date];
+                    const formattedData = {
+                        company: company,
+                        date: date,
+                        open: priceData['1. open'],
+                        high: priceData['2. high'],
+                        low: priceData['3. low'],
+                        close: priceData['4. close'],
+                        volume: priceData['5. volume']
+                    };
+                    stockDataResults.push(formattedData);
+                }
             }
+        } else {
+            console.log(`No daily data available for ${company}`);
         }
     }
+
+    const filePath = path.join(__dirname, 'StockData.json');
+    fs.writeFileSync(filePath, JSON.stringify(stockDataResults, null, 2));
+    console.log(`Data written to ${filePath}`);
 }
 
 main();
